5bafa702906d25ff44ab75b3ca5d4ebe2b7df2e6
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-30 21:11:31 -0700

    Implement ContactsList class

--- contact_list.py
+++ contact_list.py
@@ -1,1 +1,37 @@
-class ContactList():
+
+from dataclasses import dataclass, field
+
+# TODO set for shared contacts
+# TODO heap for contacts
+
+
+@dataclass
+class ContactList:
+    name: str
+    contacts: list = field(default_factory=list)
+
+    def add_contact(self, contact):
+        self.contacts.append(contact)
+        self.contacts.sort(key=lambda c: c['name'])
+
+    def remove_contact(self, name):
+        self.contacts = [c for c in self.contacts if c['name'] != name]
+
+    def find_shared_contacts(self, other_list):
+        return [c for c in self.contacts if c in other_list.contacts]
+
+
+def main():
+    friends = [{'name': 'Alice', 'number': '867-5309'}, {'name': 'Bob', 'number': '555-5555'}]
+    work_buddies = [{'name': 'Alice', 'number': '867-5309'}, {'name': 'Carlos', 'number': '555-5555'}]
+
+    my_friends_list = ContactList('My Friends', friends)
+    print(my_friends_list)
+
+    my_work_buddies = ContactList('Work Buddies', work_buddies)
+    print(my_work_buddies)
+
+    friends_i_work_with = my_friends_list.find_shared_contacts(my_work_buddies)
+    # friends_i_work_with should be: [{'name':'Alice','number':'867-5309'}]
+
+    print(friends_i_work_with)
