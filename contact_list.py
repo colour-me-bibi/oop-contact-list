ab4b3dca0684c8c5232bb1e92c34118e54c5f400
colour-me-bibi <bennyforeman1@gmail.com>
2022-05-30 21:21:29 -0700

    Fix find method

--- contact_list.py
+++ contact_list.py
@@ -6,32 +6,43 @@ from dataclasses import dataclass, field
 
 
 @dataclass
+class Contact:
+    name: str
+    number: str
+
+
+@dataclass
 class ContactList:
     name: str
-    contacts: list = field(default_factory=list)
+    contacts: list[Contact] = field(default_factory=list)
 
     def add_contact(self, contact):
         self.contacts.append(contact)
-        self.contacts.sort(key=lambda c: c['name'])
+        self.contacts.sort(key=lambda c: c["name"])
 
     def remove_contact(self, name):
-        self.contacts = [c for c in self.contacts if c['name'] != name]
+        self.contacts = [c for c in self.contacts if c["name"] != name]
 
     def find_shared_contacts(self, other_list):
-        return [c for c in self.contacts if c in other_list.contacts]
+        others = set(contact["name"] for contact in other_list.contacts)
+        return [contact for contact in self.contacts if contact["name"] in others]
 
 
 def main():
-    friends = [{'name': 'Alice', 'number': '867-5309'}, {'name': 'Bob', 'number': '555-5555'}]
-    work_buddies = [{'name': 'Alice', 'number': '867-5309'}, {'name': 'Carlos', 'number': '555-5555'}]
+    friends = [{"name": "Alice", "number": "867-5309"}, {"name": "Bob", "number": "555-5555"}]
+    work_buddies = [{"name": "Alice", "number": "867-5309"}, {"name": "Carlos", "number": "555-5555"}]
 
-    my_friends_list = ContactList('My Friends', friends)
+    my_friends_list = ContactList("My Friends", friends)
     print(my_friends_list)
 
-    my_work_buddies = ContactList('Work Buddies', work_buddies)
+    my_work_buddies = ContactList("Work Buddies", work_buddies)
     print(my_work_buddies)
 
     friends_i_work_with = my_friends_list.find_shared_contacts(my_work_buddies)
-    # friends_i_work_with should be: [{'name':'Alice','number':'867-5309'}]
+    # friends_i_work_with should be: [{"name":"Alice","number":"867-5309"}]
 
     print(friends_i_work_with)
+
+
+if __name__ == "__main__":
+    main()
