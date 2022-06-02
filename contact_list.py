6739788a3283bb67ea6b9c6ff5f3ccc25fec16c0
colour-me-bibi <bennyforeman1@gmail.com>
2022-06-01 22:38:56 -0700

    Clarify variable names

--- contact_list.py
+++ contact_list.py
@@ -18,10 +18,10 @@ class ContactList:
 
     def add_contact(self, contact):
         self.contacts.append(contact)
-        self.contacts.sort(key=lambda c: c["name"])
+        self.contacts.sort(key=lambda contact: contact["name"])
 
     def remove_contact(self, name):
-        self.contacts = [c for c in self.contacts if c["name"] != name]
+        self.contacts = [contact for contact in self.contacts if contact["name"] != name]
 
     def find_shared_contacts(self, other_list):
         return ContactList(
@@ -34,16 +34,31 @@ def main():
     friends = [{"name": "Alice", "number": "867-5309"}, {"name": "Bob", "number": "555-5555"}]
     work_buddies = [{"name": "Alice", "number": "867-5309"}, {"name": "Carlos", "number": "555-5555"}]
 
-    my_friends_list = ContactList("My Friends", friends)
+    my_friends_list = ContactList("My Friends", [Contact(**contact) for contact in friends])
     print(my_friends_list)
 
-    my_work_buddies = ContactList("Work Buddies", work_buddies)
+    my_work_buddies = ContactList("Work Buddies", [Contact(**contact) for contact in work_buddies])
     print(my_work_buddies)
 
     friends_i_work_with = my_friends_list.find_shared_contacts(my_work_buddies)
     # friends_i_work_with should be: [{"name":"Alice","number":"867-5309"}]
 
     print(friends_i_work_with)
+
+    """
+    ContactList(
+        name='My Friends',
+        contacts=[Contact(name='Alice', number='867-5309'), Contact(name='Bob', number='555-5555')],
+    )
+    ContactList(
+        name='Work Buddies',
+        contacts=[Contact(name='Alice', number='867-5309'), Contact(name='Carlos', number='555-5555')],
+    )
+    ContactList(
+        name='My Friends & Work Buddies',
+        contacts=[Contact(name='Alice', number='867-5309')],
+    )
+    """
 
 
 if __name__ == "__main__":
